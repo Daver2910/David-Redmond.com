2e61080dbb5e8e8633f65a7a87cbdef91fcbf9b8
David Redmond <mail.davidredmond@gmail.com>
2018-05-24 20:33:47 +0200

    This is the update to version 1.0.0 to be delopyed

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const path = require('path');
+const nodemailer = require('nodemailer');
 
 const app = express();
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "views"));
@@ -44,6 +46,60 @@ app.use(expressValidator({
 }));
 
 
+// Nodemailer Setup!
+app.post('/', (req, res)=>{
+    res.render( "base", {
+      title : "David Redmond Web Developer"
+    });
+    
+  let output =`<h4>You have a new message from the contact form</h4>
+  <ul>
+    <li>Name : ${req.body.name}</li>
+    <li>Email : ${req.body.email}</li>
+    <li>Phone No. : ${req.body.phone}</li>
+  </ul>
+  <H4>Message</h4>
+  <p> ${req.body.message}</p>`;
+
+      nodemailer.createTestAccount((err, account) => {
+          // create reusable transporter object using the default SMTP transport
+          let transporter = nodemailer.createTransport({
+              host: 'smtp.david-redmond.com',
+              port: 587,
+              secure: false, // true for 465, false for other ports
+              auth: {
+                  user: '1234@david-redmond.com', // generated ethereal user
+                  pass: 'Dave3208!' // generated ethereal password
+              },
+              tls: {
+                rejectUnauthorized: false
+              }
+            });
+          // setup email data with unicode symbols
+          let mailOptions = {
+              from: '"Node Application" <1234@david-redmond.com>', // sender address
+              to: 'mail.davidredmond@gmail.com', // list of receivers
+              subject: '*** NEW contact from Website***', // Subject line
+              text: 'Hello world?', // plain text body
+              html: output // html body - this is the var created above
+          };
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+            res.render("base", {      title: "David Redmond Web Developer"} );
+
+
+            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+            });
+        });
+});
+
+//
 // VIEWS  and URL Directions
 app.get('/', function(req, res) {
     res.render('base', {      title: "David Redmond Web Developer"} );
